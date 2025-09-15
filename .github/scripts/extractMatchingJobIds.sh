0508a0ff20538c49e74191ff442d7daad86a91d0
G'lek Tarssza <glek@glektarssza.com>
2025-09-15 08:24:48 -0600

    Small refactor.

--- .github/scripts/extractMatchingJobIds.sh
+++ .github/scripts/extractMatchingJobIds.sh
@@ -21,15 +21,15 @@ function get_script_dir() {
 
 # -- Forward declare variables
 declare -a JOB_IDS;
-declare SCRIPT_DIR PROJECT_ROOT STATUS_CODE HEAD_REF HEAD_SHA DRY_RUN;
-declare JOB_NAME_PATTERN;
+declare SCRIPT_DIR PROJECT_ROOT STATUS_CODE;
+declare REPOSITORY JOB_NAME_PATTERN DRY_RUN
 
 # -- Cleanup routine
 # shellcheck disable=SC2329
 function cleanup() {
     unset JOB_IDS;
-    unset SCRIPT_DIR PROJECT_ROOT STATUS_CODE HEAD_REF HEAD_SHA DRY_RUN;
-    unset JOB_NAME_PATTERN;
+    unset SCRIPT_DIR PROJECT_ROOT STATUS_CODE;
+    unset REPOSITORY JOB_NAME_PATTERN DRY_RUN
 }
 
 trap cleanup EXIT;
@@ -53,14 +53,14 @@ function parse_args() {
     echo "::debug::Parsing arguments...";
     while [[ -n "$1" ]]; do
         case "$1" in
-            --head-ref)
+            --repo|-r)
                 shift 1;
-                HEAD_REF="$1";
-                echo "::debug::Parsed head reference \"${HEAD_REF}\"";
+                REPOSITORY="$1";
+                echo "::debug::Parsed GitHub repository \"${REPOSITORY}\"";
             ;;
-            --head-ref=*)
-                HEAD_REF="$(echo "$1" | awk -F"=" '{print $2;}')";
-                echo "::debug::Parsed head reference \"${HEAD_REF}\"";
+            --repo=*)
+                REPOSITORY="$(echo "$1" | awk -F"=" '{print $2;}')";
+                echo "::debug::Parsed GitHub run ID \"${REPOSITORY}\"";
             ;;
             --run-id)
                 shift 1;
@@ -78,6 +78,15 @@ function parse_args() {
             ;;
             --job-name-pattern=*)
                 JOB_NAME_PATTERN="$(echo "$1" | awk -F"=" '{print $2;}')";
+                echo "::debug::Parsed job name pattern \"${JOB_NAME_PATTERN}\"";
+            ;;
+            --job-name)
+                shift 1;
+                JOB_NAME_PATTERN="^$(echo "$1" | sed -E 's/([][)(\\\|\*\+\?\^\$\.\!}{])/\\\1/g')$";
+                echo "::debug::Parsed job name pattern \"${JOB_NAME_PATTERN}\"";
+            ;;
+            --job-name=*)
+                JOB_NAME_PATTERN="^$(echo "$1" | awk -F"=" '{print $2;}' | sed -E 's/([][)(\\\|\*\+\?\^\$\.\!}{])/\\\1/g')$";
                 echo "::debug::Parsed job name pattern \"${JOB_NAME_PATTERN}\"";
             ;;
             --dry-run)
@@ -104,6 +113,10 @@ function parse_args() {
 
 parse_args "$@";
 
+if [[ -z "${REPOSITORY}" ]]; then
+    REPOSITORY="${OWNER}/${REPO}";
+fi
+
 if [[ -z "${RUN_ID}" ]]; then
     echo "::error::Invalid GitHub run ID (no value)!";
     exit 1;
@@ -119,18 +132,6 @@ if [[ -z "${JOB_NAME_PATTERN}" ]]; then
     exit 1;
 fi
 
-if [[ -z "${HEAD_REF}" ]]; then
-    echo "::error::Invalid head reference (no value)!";
-    exit 1;
-fi
-
-HEAD_SHA="$(git rev-parse --verify "${HEAD_REF}" 2> /dev/null)";
-
-if [[ -z "${HEAD_SHA}" ]]; then
-    echo "::error::Invalid head reference (does not map to a known Git SHA)!";
-    exit 1;
-fi
-
 echo "::debug::Calling GitHub API to retrieve known check runs for Git SHA \"${HEAD_SHA}\"...";
 if is_dry_run; then
     STATUS_CODE="0";
@@ -138,13 +139,18 @@ else
     GITHUB_API_CALL_DATA="$(gh api --method GET \
         -H "Accept: application/vnd.github+json" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        "/repos/${OWNER}/${REPO}/actions/runs/${RUN_ID}/jobs" | \
+        "/repos/${REPOSITORY}/actions/runs/${RUN_ID}/jobs" | \
         jq --arg job_name "${JOB_NAME_PATTERN}" -f "${SCRIPT_DIR}/workflow_runs_extract_job_ids.jq")";
     STATUS_CODE="$?";
 fi
 if [[ "${STATUS_CODE}" != "0" ]]; then
     echo "::error::Failed to call GitHub API (call returned status code \"${STATUS_CODE}\")!";
     exit "${STATUS_CODE}";
+fi
+
+if [[ -z "${GITHUB_API_CALL_DATA}" ]]; then
+    echo "::error::GitHub API returned no results for job name pattern \"${JOB_NAME_PATTERN}\"!";
+    exit 1;
 fi
 
 mapfile -t JOB_IDS < <(echo "${GITHUB_API_CALL_DATA}");
