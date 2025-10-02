54fab020766a846e0896633d9cab0a0009c40385
G'lek Tarssza <glek@glektarssza.com>
2025-10-02 13:55:08 -0600

    Some more common errors.

--- .github/scripts/lib/errors.ts
+++ .github/scripts/lib/errors.ts
@@ -3,7 +3,13 @@
  * @enum
  */
 export const ErrorCodesEnum = {
-    OctokitNotInitialized: 'E_OCTOKIT_NOT_INITIALIZED'
+    OctokitNotInitialized: 'E_OCTOKIT_NOT_INITIALIZED',
+    OctokitNetFetchFailed: 'E_OCTOKIT_NET_FETCH_FAILED',
+    OctokitNetFetchNoResults: 'E_OCTOKIT_NET_NO_RESULTS',
+    OctokitCheckRunCreateFailed: 'E_OCTOKIT_CHECK_RUN_CREATE_FAILED',
+    OctokitCheckRunUpdateFailed: 'E_OCTOKIT_CHECK_RUN_UPDATE_FAILED',
+    ExecFailedNonZeroStatus: 'E_EXEC_FAILED_NON_ZERO_STATUS',
+    ExecFailedStderrHasContents: 'E_EXEC_FAILED_STDERR_HAS_CONTENTS'
 } as const;
 
 /**
