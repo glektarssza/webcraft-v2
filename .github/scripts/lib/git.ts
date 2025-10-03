8d8f6c2ee5c5e314d9c9cdfaff9f13da9b7a5867
G'lek Tarssza <glek@glektarssza.com>
2025-10-03 07:41:33 -0600

    Make sure we trim outputs of commands before returning.

--- .github/scripts/lib/git.ts
+++ .github/scripts/lib/git.ts
@@ -16,5 +16,21 @@ export async function revParse(
     if (result.stderr) {
         throw new Error(ErrorCodesEnum.ExecFailedStderrHasContents);
     }
-    return result.stdout;
+    return result.stdout.trim();
+}
+
+export async function getCurrentBranch(
+    execModule: ActionsExecModule
+): Promise<string> {
+    const result = await execModule.getExecOutput('git', [
+        'branch',
+        '--show-current'
+    ]);
+    if (result.exitCode !== 0) {
+        throw new Error(ErrorCodesEnum.ExecFailedNonZeroStatus);
+    }
+    if (result.stderr) {
+        throw new Error(ErrorCodesEnum.ExecFailedStderrHasContents);
+    }
+    return result.stdout.trim();
 }
