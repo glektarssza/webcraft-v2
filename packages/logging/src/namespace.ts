34568358fdcc416e564876a9eb4a4e120e7a024f
G'lek Tarssza <glek@glektarssza.com>
2026-07-28 10:58:15 -0600

    Namespace functions.

--- packages/logging/src/namespace.ts
+++ packages/logging/src/namespace.ts
@@ -1,17 +1,46 @@
 /**
- * A module which handles logging namespaces and related logic.
+ * A module which provides methods for manipulating logging namespaces.
  *
  * @module
  */
 
-import {type Distinct} from '@webcraft/core';
+import {Namespace, NamespaceComponent} from './types';
 
 /**
- * A logging namespace.
+ * The character used to separate logging namespaces in a list of namespaces.
  */
-export type Namespace = Distinct<string, 'Namespace'>;
+export const NAMESPACE_SEPARATOR = ',';
 
 /**
- * A logging namespace component.
+ * The character used to separate namespace components in a logging namespace.
  */
-export type NamespaceComponent = Distinct<string, 'NamespaceComponent'>;
+export const NAMESPACE_COMPONENT_SEPARATOR = ':';
+
+/**
+ * Check if a value is a logging namespace.
+ *
+ * @param value The value to check.
+ *
+ * @returns `true` if the value is a logging namespace, `false` otherwise.
+ */
+export const isNamespace = (value: unknown): value is Namespace => {
+    return typeof value === 'string' && !value.includes(NAMESPACE_SEPARATOR);
+};
+
+/**
+ * Check if a value is a logging namespace component.
+ *
+ * @param value The value to check.
+ *
+ * @returns `true` if the value is a logging namespace component, `false`
+ * otherwise.
+ */
+export const isNamespaceComponent = (
+    value: unknown
+): value is NamespaceComponent => {
+    return (
+        typeof value === 'string' &&
+        !value.includes(NAMESPACE_SEPARATOR) &&
+        !value.includes(NAMESPACE_COMPONENT_SEPARATOR)
+    );
+};
